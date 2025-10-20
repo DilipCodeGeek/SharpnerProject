4ddd5443f050856bd09931e41d62b7797c0de3ed
DilipCodeGeek <dilipnair85@gmail.com>
2025-10-20 17:10:42 +0530

    Update: DOM manipulation with getElementsByTagName

--- UnderstandingDom/index.js
+++ UnderstandingDom/index.js
@@ -1,6 +1,6 @@
 // Write your code below:
-const fruit = document.getElementsByClassName("fruit");
-fruit[2].style.backgroundColor = "yellow";
-for (let i = 0; i < fruit.length; i++) {
-    fruit[i].style.fontWeight = "bold";
+const listItem = document.getElementsByTagName("li");
+listItem[4].style.color = "blue";
+for (let i = 0; i < listItem.length; i++){
+    listItem[i].style.fontStyle = "italic";
 }
