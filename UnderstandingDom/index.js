0f26447417f2e8b8a0764976f3272c6b193aa302
DilipCodeGeek <dilipnair85@gmail.com>
2025-10-20 20:23:32 +0530

    Update code using querySelector and querySelectorAll methods

--- UnderstandingDom/index.js
+++ UnderstandingDom/index.js
@@ -1,6 +1,29 @@
-// Write your code below:
-const listItem = document.getElementsByTagName("li");
-listItem[4].style.color = "blue";
-for (let i = 0; i < listItem.length; i++){
-    listItem[i].style.fontStyle = "italic";
+// Write the code as shown in the video below:
+const mainHeading = document.querySelector("#main-heading");
+mainHeading.style.textAlign = "center";
+
+const fruits = document.querySelector(".fruits");
+fruits.style.backgroundColor = "grey";
+fruits.style.padding = "30px";
+fruits.style.margin = "30px";
+fruits.style.width = "50%";
+fruits.style.borderRadius = "5px";
+fruits.style.listStyleType = "none";
+
+const basketHeading = document.querySelector("#basket-heading");
+basketHeading.style.marginLeft = "30px";
+
+const fruitItems = document.querySelectorAll(".fruit");
+for (let i = 0; i < fruitItems.length; i++){
+    fruitItems[i].style.backgroundColor = "white";
+    fruitItems[i].style.padding = "10px";
+    fruitItems[i].style.margin = "10px";
+    fruitItems[i].style.borderRadius = "5px";
+}
+// Write answer to the questions asked below:
+basketHeading.style.color = "brown";
+const evenFruitItems = document.querySelectorAll(".fruit:nth-child(even)");
+for (let i = 0; i < evenFruitItems.length; i++){
+    evenFruitItems[i].style.backgroundColor = "brown";
+    evenFruitItems[i].style.color = "white";
 }
